736484fbc1e523f2c3b5f0083762f2f13dca79d7
sharkk121 <11296288+sharkk121@users.noreply.github.com>
2024-10-10 19:03:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace EfCoreBug
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public ICollection<Tag>? Tags { get; set; }
+        public ICollection<PostTag>? PostTags { get; set; }
+    }
+
+    public class Tag
+    {
+        public int Id { get; set; }
+        public ICollection<Post>? Posts { get; set; }
+        public ICollection<PostTag>? PostTags { get; set; }
+    }
+
+    public class PostTag
+    {
+        //public int PostId { get; set; }
+        //public int TagId { get; set; }
+        public Post? Post { get; set; }
+        public Tag? Tag { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ TestDbContext.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EfCoreBug
+{
+    internal class TestDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(PostsConfiguration).Assembly);
+        }
+    }
+
+    internal class PostsConfiguration : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder.ToTable("Posts");
+
+            builder.HasKey(x => x.Id);
+
+            builder
+                .HasMany(e => e.Tags)
+                .WithMany(e => e.Posts)
+                .UsingEntity<PostTag>(
+                    l => l
+                        .HasOne(x => x.Tag)
+                        .WithMany(x => x.PostTags)
+                        //.HasForeignKey(x => x.TagId)
+                        .HasForeignKey("TagId")
+                        .OnDelete(DeleteBehavior.Cascade),
+                    r => r
+                        .HasOne(x => x.Post)
+                        .WithMany(x => x.PostTags)
+                        //.HasForeignKey(x => x.PostId)
+                        .HasForeignKey("PostId")
+                        .OnDelete(DeleteBehavior.Restrict),
+                    j => j.ToTable("PostTags"));
+        }
+    }
+
+    internal class TagsConfiguration : IEntityTypeConfiguration<Tag>
+    {
+        public void Configure(EntityTypeBuilder<Tag> builder)
+        {
+            builder.ToTable("Tags");
+
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
